f068378eae4652a63c1d14238d9eede677a6057f
Asim Poudel <bipulp49@gmail.com>
2023-05-17 11:30:42 +0545

    adding build ((frontend))

---
+++ build/precache-manifest.5a3a9a967dbae080d10ab847c4db9c37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aff0bb3afb99691e63ef",
+    "url": "/static/css/main.ed9cc99a.chunk.css"
+  },
+  {
+    "revision": "aff0bb3afb99691e63ef",
+    "url": "/static/js/main.aff0bb3a.chunk.js"
+  },
+  {
+    "revision": "f8e2d5af23cfa5709471",
+    "url": "/static/js/1.f8e2d5af.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cb39117e3cceed8a7bc312a71cda8a68",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.d908b7b9a71f4bfa8de2e1ac38adeede.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "473a5c8b8e35934de0f4",
+    "url": "/static/css/main.ed9cc99a.chunk.css"
+  },
+  {
+    "revision": "473a5c8b8e35934de0f4",
+    "url": "/static/js/main.473a5c8b.chunk.js"
+  },
+  {
+    "revision": "f8e2d5af23cfa5709471",
+    "url": "/static/js/1.f8e2d5af.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c95eece71204198c4af95580d00ba54e",
+    "url": "/index.html"
+  }
+];
